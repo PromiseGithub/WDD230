ac6c5a52243bd92b63fdf812a178159880337d00
oko18002@byui.edu <73972260+PromiseGithub@users.noreply.github.com>
2022-07-21 06:51:37 -0600

    Temple Inn

--- templeinn/scripts/currentDate.js
+++ templeinn/scripts/currentDate.js
@@ -28,3 +28,25 @@ let y = d.getFullYear();
 let m = month[d.getMonth()];
 
 document.getElementById("currentDate").innerHTML = m + " " + x + ", " + y;
+
+// current waether
+
+const url = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=901fd2bc31eca7e962727acbae06da9f&units=imperial"
+
+fetch(url).then(response => response.json()).then(data => {
+	console.log(data)
+	let currenttemp = data.main.temp;
+	let currcond = data.weather[0].description;
+	let icon = data.weather[0].icon;
+	let speed = data.wind.speed;
+	let chill = Math.round(35.74 + (0.6215 * currenttemp) - Math.pow(35.75 * speed, 0.16) + Math.pow(0.4275 * currenttemp * speed, 0.16));
+	console.log(speed);
+	
+	document.querySelector("#curr-temp").innerText = currenttemp;
+	document.querySelector("#curr-cond").innerText = currcond;
+	document.querySelector("#windspeed").innerText = speed;
+	document.querySelector("#windchill").innerText = chill;
+
+	let iconSource = `http://openweathermap.org/img/wn/${icon}@2x.png`
+	document.querySelector("#icon").setAttribute('src', iconSource);
+})
